989bab557d71c1d65330ed009b103c5de1904658
Fares Ben Hamouda <fares.benhamouda@tvsmiles.de>
2019-10-18 01:15:02 +0200

    Initial Commit

---
+++ KwizzadRTASample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KwizzadRTASample
+//
+//  Created by Fares Ben Hamouda on 18.10.19.
+//  Copyright © 2019 Fares Ben Hamouda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KwizzadRTASampleTests/KwizzadRTASampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  KwizzadRTASampleTests.swift
+//  KwizzadRTASampleTests
+//
+//  Created by Fares Ben Hamouda on 18.10.19.
+//  Copyright © 2019 Fares Ben Hamouda. All rights reserved.
+//
+
+import XCTest
+@testable import KwizzadRTASample
+
+class KwizzadRTASampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
